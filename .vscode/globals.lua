73621431d41d53e28af32ae176a949dbbdfaf014
Tubbles <jae91m@gmail.com>
2021-11-20 19:25:28 +0100

    Add lua file with global definitions for intellisense

---
+++ .vscode/globals.lua
@@ -0,0 +1,18 @@
+-- Define the globals from Factorio so intellisense doesn't spew all over the code base
+-- objects
+commands = nil
+data = nil
+defines = nil
+game = nil
+global = nil
+rcon = nil
+remote = nil
+rendering = nil
+script = nil
+SD = nil
+settings = nil
+
+-- function
+localised_print = function(str) end
+log = function(str) end
+table_size = function(obj) end
